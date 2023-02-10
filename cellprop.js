94971f95e7e76bb282e19cce5cda28b855398df9
Mayur199627 <mayur27asati@gmail.com>
2023-02-10 08:40:12 +0530

    save

--- cellprop.js
+++ cellprop.js
@@ -8,7 +8,7 @@ for (let i = 0; i < rows; i++) {
             italic: false,
             underline: false,
             alignment: "left",
-            fontfamily: "monospace",
+            fontFamily: "monospace",
             fontSize: 14,
             fontColor: "#000000",
             fontbgColor: "#000000"
@@ -31,7 +31,7 @@ let leftAlignment = document.querySelector('img[alt="left-align"]')
 let rightAlignment = document.querySelector('img[alt="right-align"]')
 let justifytext = document.querySelector('img[alt="justify"]')
 
-let activecolorProp = "gray"
+let activecolorProp = "#b2bec3"
 let inactivecolorProp = "transparent"
 
 // application of two way binding
@@ -44,6 +44,75 @@ bold.addEventListener("click",(e)=>{
     cellProp.bold = !cellProp.bold
     cell.style.fontWeight = cellProp.bold ? "bold" : "normal"; //UI change
     bold.style.backgroundColor = cellProp.bold ? activecolorProp : inactivecolorProp;
+})
+
+italic.addEventListener("click",(e)=>{
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address)
+
+    //modifiaction
+    cellProp.italic = !cellProp.italic
+    cell.style.fontStyle = cellProp.italic? "italic" : "normal"; //UI change
+    italic.style.backgroundColor = cellProp.italic ? activecolorProp : inactivecolorProp;
+})
+
+
+underline.addEventListener("click",(e)=>{
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address)
+
+    //modifiaction
+    cellProp.underline = !cellProp.underline
+    cell.style.textDecoration = cellProp.underline? "underline" : "none"; //UI change
+    underline.style.backgroundColor = cellProp.underline ? activecolorProp : inactivecolorProp;
+})
+
+fontSize.addEventListener("click",()=>{
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address)
+
+    cellProp.fontSize = fontSize.value;
+    cell.style.fontSize = cellProp.fontSize + "px"
+    fontSize.value = cellProp.fontSize
+})
+
+
+fontFamily.addEventListener("click",()=>{
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address)
+
+    cellProp.fontFamily = fontFamily.value;
+    cell.style.fontFamily = cellProp.fontFamily 
+    fontFamily.value = cellProp.fontFamily
+})
+
+fontColor.addEventListener('click',()=>{
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address)
+
+    cellProp.fontColor = fontColor.value;
+    cell.style.color = cellProp.fontColor 
+    fontColor.value = cellProp.fontColor
+})
+
+
+bgColor.addEventListener('click',()=>{
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address)
+
+    cellProp.fontbgColor = fontbgColor.value;
+    cell.style.backgroundColor = cellProp.fontbgColor 
+    fontbgColor.value = cellProp.fontbgColor
+})
+
+rightAlignment.addEventListener("click", ()=>{
+    let address = addressBar.value;
+    let [cell, cellProp] = activeCell(address)
+
+    cellProp.alignment = rightAlignment.value;
+    cell.style.textAlign = cellProp.alignment 
+    rightAlignment.value = cellProp.alignment
+    
 })
 
 function activeCell(address){
